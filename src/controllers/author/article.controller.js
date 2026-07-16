95ce89051a6c38202555693ded2604aaf7ece818
Aadit yadav <yaaditya191@gmail.com>
2026-07-17 00:37:28 +0545

    FEAT(Author):update Author controller and route

---
+++ src/controllers/author/article.controller.js
@@ -0,0 +1,150 @@
+const Article = require("../../models/Article.models");
+
+// Author creates a new article 
+const createArticle = async (req, res) => {
+  try {
+    const { title, content , featuredImage } = req.body;
+    
+    if (!title?.trim()) {
+      return res.status(400).json({
+        message: "Title is required.",
+      });
+    }
+
+    if (!content?.trim()) {
+      return res.status(400).json({
+        message: "Content is required.",
+      });
+    }
+
+    if (!req.file) {
+      return res.status(400).json({
+        message: "Please upload a featured image banner.",
+      });
+    }
+
+    // Generate slug
+    const slug = `${title
+      .toLowerCase()
+      .trim()
+      .replace(/[^a-z0-9]+/g, "-")
+      .replace(/^-|-$/g, "")}-${Date.now()}`;
+
+    const existingArticle = await Article.findOne({
+      title: title.trim(),
+      author: req.user.id,
+      status: "PENDING",
+    });
+    if (existingArticle) {
+      return res.status(409).json({
+        message: "You already have an article with this title.",
+      });
+    }
+
+    // Save article
+    const article = await Article.create({
+      title: title.trim(),
+      slug,
+      content: content.trim(),
+      featuredImage: req.file.path,
+      author: req.user.id,
+      status: "PENDING",
+    });
+
+    const populatedArticle = await Article.findById(article._id).populate(
+      "author",
+      "name",
+    );
+
+    return res.status(201).json({
+      success: true,
+      message: "Article created successfully and sent for review!",
+      data: populatedArticle,
+    });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Failed to create article.",
+      error: error.message,
+    });
+  }
+};
+
+// // Author submits PENDING for review
+// const submitForReview = async (req, res) => {
+//   try {
+//     const article = await Article.findOneAndUpdate(
+//       { _id: req.params.id, author: req.user.id, status: "PENDING" },
+//       { status: "PENDING" },
+//       { new: true },
+//     );
+
+//     if (!article) {
+//       return res.status(404).json({ message: "Article not found" });
+//     }
+//     const populatedArticle = await article.populate("author", "name");
+//     res.status(200).json({
+//       message: "Submitted for review successfully",
+//       article: populatedArticle,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+
+// //Get all APPROVED articles
+// const getPublicArticles = async (req, res) => {
+//   try {
+//     const page = parseInt(req.query.page) || 1;
+//     const limit = parseInt(req.query.limit) || 10;
+//     const skip = (page - 1) * limit;
+
+//     //query for status: 'APPROVED'
+//     const articles = await Article.find({ status: "APPROVED" })
+//       .select("title slug featuredImage APPROVEDAt summary")
+//       .populate("author", "name")
+//       .sort({ APPROVEDAt: -1 })
+//       .skip(skip)
+//       .limit(limit);
+
+//     const totalArticles = await Article.countDocuments({ status: "APPROVED" });
+
+//     res.status(200).json({
+//       page,
+//       totalPages: Math.ceil(totalArticles / limit),
+//       results: articles.length,
+//       data: articles,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+// // Get a single APPROVED article by slug
+// const getArticleBySlug = async (req, res) => {
+//   try {
+//     const { slug } = req.params;
+
+//     const article = await Article.findOne({
+//       slug,
+//       status: "APPROVED",    //Not Showing to unAPPROVED articles i.e. PENDING or PENDING
+//     }).populate("author", "name");
+
+//     if (!article) {
+//       return res.status(404).json({ message: "Article not found" });
+//     }
+
+//     res.status(200).json({ data: article });
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// };
+
+module.exports = {
+  createArticle,
+  // submitForReview,
+  // getPublicArticles,
+  // getArticleBySlug,
+};
---
+++ src/controllers/author/auth.controller.js
@@ -0,0 +1,154 @@
+const User = require("../../models/user.models.js");
+const generateToken = require("../../utils/jwt.js");
+const { hashPassword, comparePassword } = require("../../utils/password.js");
+const logger = require("../../utils/logger.js");
+
+ 
+// Author Signup
+const authorsignup = async (req, res) => {
+  try {
+    const { name, email, password , } = req.body;
+    if (!name || !email || !password ) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all required fields.",
+      });
+    }
+
+    // Check existing user
+    const existingUser = await User.findOne({ email });
+
+    if (existingUser) {
+      return res.status(400).json({
+        success: false,
+        message: "User already registered with this email.",
+      });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    // Create AUTHOR only
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      role: "AUTHOR",
+      status: "PENDING",
+      isActive: false,
+    });
+
+    res.status(201).json({
+      success: true,
+      message:
+        "Registration successful. Please wait for admin approval before logging in.",
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        status: user.status,
+      },
+    });
+  } catch (error) {
+    logger.error("Author signup failed", {
+      message: error.message,
+      stack: error.stack,
+    });
+
+    res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+ 
+// Author Login
+const authorlogin = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Please fill all fields.",
+      });
+    }
+
+    // Find only AUTHOR
+    const user = await User.findOne({
+      email,
+      role: "AUTHOR",
+    }).select("+password");
+
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials.",
+      });
+    }
+
+    // Compare password
+    const isMatch = await comparePassword(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid credentials.",
+      });
+    }
+
+    // Check approval
+    if (user.status !== "APPROVED") {
+      return res.status(403).json({
+        success: false,
+        message: "Your account has not been approved yet to Login.",
+      });
+    }
+
+    // Check active status
+    if (!user.isActive) {
+      return res.status(403).json({
+        success: false,
+        message: "Your account is inactive. Please contact the administrator.",
+      });
+    }
+
+    // Generate JWT
+    const token = generateToken({
+      id: user._id,
+      role: user.role,
+    });
+
+    user.password = undefined;
+
+    return res.status(200).json({
+      success: true,
+      message: "Logged in successfully.",
+      token,
+      user: {
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        role: user.role,
+        status: user.status,
+        isActive: user.isActive,
+      },
+    });
+  } catch (error) {
+    logger.error("Author login failed", {
+      message: error.message,
+      stack: error.stack,
+    });
+
+    return res.status(500).json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+module.exports = {
+  authorsignup,
+  authorlogin,
+};
