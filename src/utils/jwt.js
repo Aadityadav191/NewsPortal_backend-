5907628e5bee569f3f6e4ecbc8135a4637c9919f
Aadit yadav <yaaditya191@gmail.com>
2026-07-16 21:48:19 +0545

    FIX: fixed super admin not found error

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,8 +1,8 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, {
+const generateToken = (user) => {
+  return jwt.sign({ id: user._id , role: user.role }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN || '1d',
   });
 };
